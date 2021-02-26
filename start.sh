37cd003587085fb81eb9168928cf11c7a183916d
nicolassims <sableye.nico@gmail.com>
2021-02-26 14:17:43 -0800

    Updated tertiary files with new port and paths.

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
-export PORT=4780
+export PORT=4800
 
 echo "Stopping old copy of app, if any..."
 
-_build/prod/rel/bulls/bin/bulls stop || true
+_build/prod/rel/multibulls/bin/multibulls stop || true
 
 echo "Starting app..."
 
-_build/prod/rel/bulls/bin/bulls start
+_build/prod/rel/multibulls/bin/multibulls start
